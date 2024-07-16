97e3f86a0436bacef48b0e9f6cdc97f5fb1d5e03
omiras <oscar.miras@gmail.com>
2024-07-16 09:08:36 +0200

    done

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get('/api/get-computed-results', (req, res) => {
 });
 
 // Levantar el servidor
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000.");
 });
 
